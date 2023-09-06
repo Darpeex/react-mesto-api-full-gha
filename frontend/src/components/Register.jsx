2319c9c824985231923b574c36728ea43d2efb72
Darpeex <foxyyysp@mail.ru>
2023-09-06 18:36:07 +0300

    refactor: изменено сообщение ошибки при регистрации х3

--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -26,7 +26,7 @@ export const Register = ({ onInfoTooltip, onResult, errorMessage }) => {
       navigate('/sign-in', {replace: true});
     }).catch((err) => {
       onResult(false)
-      errorMessage('Ошибка, возможно почта зарегистрированна')
+      errorMessage('Вероятно, почта уже зарегистрированна')
     })
     .finally(onInfoTooltip)
   }
