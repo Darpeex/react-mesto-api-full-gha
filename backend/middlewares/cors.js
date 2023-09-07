bbb5c559317aa9314632810dfe104770580eb89b
Darpeex <foxyyysp@mail.ru>
2023-09-07 13:08:42 +0300

    test: тестируем заголовки cors х3

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -16,6 +16,8 @@ router.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', origin);
     // устанавливаем заголовок Access-Control-Allow-Credentials с значением true
     res.header('Access-Control-Allow-Credentials', 'true');
+    // разрешаем кросс-доменные запросы с этими методами
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   }
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
   // сохраняем список заголовков исходного запроса
