3d7c7147580c62112c363350dd5ac73ae2b47623
Darpeex <foxyyysp@mail.ru>
2023-09-07 13:24:19 +0300

    test:  заменил res.header на res.setHeader

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -13,20 +13,18 @@ router.use((req, res, next) => {
   // проверяем, что источник запроса есть среди разрешённых
   if (allowedCors.includes(origin)) {
     // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    res.header('Access-Control-Allow-Origin', origin);
+    res.setHeader('Access-Control-Allow-Origin', origin);
     // устанавливаем заголовок Access-Control-Allow-Credentials с значением true
-    res.header('Access-Control-Allow-Credentials', 'true');
-    // разрешаем кросс-доменные запросы с этими методами
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
   }
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
   // сохраняем список заголовков исходного запроса
   const requestHeaders = req.headers['access-control-request-headers'];
   if (method === 'OPTIONS') {
     // разрешаем кросс-доменные запросы с этими заголовками
-    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.setHeader('Access-Control-Allow-Headers', requestHeaders);
     // завершаем обработку запроса и возвращаем результат клиенту
-    return res.end(); // end() потому что тела ответа нет
+    return res.end(); // end() т.к. тела ответа нет
   }
   next();
 });
