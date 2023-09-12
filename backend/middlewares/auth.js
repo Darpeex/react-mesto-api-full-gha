e3a8dc47cce7aa1f61070ed054b70b3fffe1b580
Darpeex <foxyyysp@mail.ru>
2023-09-12 20:38:50 +0300

    refactor: исправлен мидлвар auth.js

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,25 +2,44 @@ const jwt = require('jsonwebtoken'); // импортируем модуль json
 
 const { NODE_ENV, JWT_SECRET } = process.env; // достали константы из .env
 
-// функции достаёт jwt из заголовка убирая 'jwt= '
 const extractJwtToken = (authorization) => authorization.replace('jwt=', '');
 const AuthorizationError = require('../errors/auth-err');
 
 module.exports = (req, res, next) => {
-  const authorization = req.headers.cookie; // из ответа получаем токен
-  // проверяем есть ли он или начинается ли с jwt= (тип токена аутентификации)
-  if (!authorization || !authorization.startsWith('jwt=')) {
-    throw new AuthorizationError('Не получен токен из ответа');
+  console.log(req.cookies);
+  const authorization = req.cookies.jwt;
+  if (!authorization) {
+    throw new AuthorizationError('Не получен токен из cookies');
   }
-  // если с полученым токеном всё в порядке
-  const token = extractJwtToken(authorization); // в переменную записывается только jwt
-  let payload; // у let блочная область видимости, чтобы payload был виден снаружи объявляем до try
-
+  const token = extractJwtToken(authorization);
+  let payload;
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'); // проверяем подпись токена и расшифровываем
-    req.user = payload; // записываем пейлоуд в объект запроса
-    next(); // пееркидываем в обработчик catch
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    req.user = payload;
+    next();
   } catch (err) {
-    next(err); // перекидываем в центролизованный обработчки
+    next(err);
   }
 };
+
+// Можно оставлю себе для примера на будущее?
+// module.exports = (req, res, next) => {
+//   const authorization = req.headers.cookie; // из ответа получаем токен
+//   // проверяем есть ли он или начинается ли с jwt= (тип токена аутентификации)
+//   if (!authorization || !authorization.startsWith('jwt=')) {
+//     throw new AuthorizationError('Не получен токен из ответа');
+//   }
+//   // если с полученым токеном всё в порядке
+//   const token = extractJwtToken(authorization); // в переменную записывается только jwt
+//   let payload; // у let блочная область видимости,
+//   // чтобы payload был виден снаружи объявляем до try
+
+//   try { // ниже проверяем подпись токена и расшифровываем
+//     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+//     req.user = payload; // записываем пейлоуд в объект запроса
+//     next(); // пееркидываем в обработчик catch
+//   } catch (err) {
+//     next(err); // перекидываем в центролизованный обработчки
+//   }
+// };
+// };
