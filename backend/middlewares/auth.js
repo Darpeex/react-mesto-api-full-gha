060799a5637ae1ecad600bbad14084527f83491d
Darpeex <foxyyysp@mail.ru>
2023-09-14 22:12:41 +0300

    refactor: почищен код и добавлены комментарии

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -5,35 +5,8 @@ const { NODE_ENV, JWT_SECRET } = process.env; // достали констант
 const extractJwtToken = (authorization) => authorization.replace('jwt=', '');
 const AuthorizationError = require('../errors/auth-err');
 
-const test = () => {
-  const YOUR_JWT = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NGZjMDU4NzkzYzRiY2U2MjZhMTcxOWYiLCJpYXQiOjE2OTQ0MzU5MjUsImV4cCI6MTY5NTA0MDcyNX0.0_NJHFF2GS8wH88_0iTwplWw02KUk3OHoBxg3rTuXTU'; // вставьте сюда JWT, который вернул публичный сервер
-  const SECRET_KEY_DEV = JWT_SECRET; // вставьте сюда секретный ключ для разработки из кода
-  try {
-    const payload2 = jwt.verify(YOUR_JWT, SECRET_KEY_DEV);
-    console.log(payload2);
-    console.log('\x1b[31m%s\x1b[0m', `
-Надо исправить. В продакшне используется тот же
-секретный ключ, что и в режиме разработки.
-`);
-  } catch (err) {
-    if (err.name === 'JsonWebTokenError' && err.message === 'invalid signature') {
-      console.log(
-        '\x1b[32m%s\x1b[0m',
-        'Всё в порядке. Секретные ключи отличаются',
-      );
-    } else {
-      console.log(
-        '\x1b[33m%s\x1b[0m',
-        'Что-то не так',
-        err,
-      );
-    }
-  }
-};
-test(); // Всё в порядке. Секретные ключи отличаются - на сервер тоже добавил, после ревью удалю)
-
 module.exports = (req, res, next) => {
-  const authorization = req.cookies.jwt;
+  const authorization = req.cookies.jwt; // записываем jwt в константу
   if (!authorization) {
     throw new AuthorizationError('Не получен токен из cookies');
   }
@@ -48,7 +21,6 @@ module.exports = (req, res, next) => {
   }
 };
 
-// Можно оставлю себе для примера на будущее?
 // module.exports = (req, res, next) => {
 //   const authorization = req.headers.cookie; // из ответа получаем токен
 //   // проверяем есть ли он или начинается ли с jwt= (тип токена аутентификации)
--- backend/middlewares/logger.js
+++ backend/middlewares/logger.js
@@ -4,17 +4,17 @@ const expressWinston = require('express-winston');
 // создадим логгер запросов
 const requestLogger = expressWinston.logger({
   transports: [
-    new winston.transports.File({ filename: 'request.log' }),
+    new winston.transports.File({ filename: 'request.log' }), // записываем запросы в файл 'request.log'
   ],
-  format: winston.format.json(),
+  format: winston.format.json(), // в формате json
 });
 
 // логгер ошибок
 const errorLogger = expressWinston.errorLogger({
   transports: [
-    new winston.transports.File({ filename: 'error.log' }),
+    new winston.transports.File({ filename: 'error.log' }), // записываем ошибки запросов в файл 'error.log'
   ],
-  format: winston.format.json(),
+  format: winston.format.json(), // в формате json
 });
 
 module.exports = {
